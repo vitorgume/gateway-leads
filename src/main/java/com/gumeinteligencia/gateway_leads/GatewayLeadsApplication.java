3503e06c084aaeeac4c1950f90632b4079211f9e
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-05-08 08:26:46 -0300

    Estrutura do projeto construida

---
+++ src/main/java/com/gumeinteligencia/gateway_leads/GatewayLeadsApplication.java
@@ -0,0 +1,13 @@
+package com.gumeinteligencia.gateway_leads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayLeadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayLeadsApplication.class, args);
+	}
+
+}
