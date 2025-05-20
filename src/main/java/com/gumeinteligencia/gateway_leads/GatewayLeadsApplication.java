41af2902870a55aa96d725b3132051547aaf7239
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-05-20 10:46:58 -0300

    Tirando importação do dotenv

--- src/main/java/com/gumeinteligencia/gateway_leads/GatewayLeadsApplication.java
+++ src/main/java/com/gumeinteligencia/gateway_leads/GatewayLeadsApplication.java
@@ -1,6 +1,5 @@
 package com.gumeinteligencia.gateway_leads;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
