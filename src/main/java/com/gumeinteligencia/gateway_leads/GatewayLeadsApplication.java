7fa7f5f40cba3f324c8a0e086ff17e1ea8d21d79
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-06-20 13:10:27 -0300

    Colocando novamente o TimeZone para o Brasil

--- src/main/java/com/gumeinteligencia/gateway_leads/GatewayLeadsApplication.java
+++ src/main/java/com/gumeinteligencia/gateway_leads/GatewayLeadsApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class GatewayLeadsApplication {
 
-	public static void main(String[] args) { SpringApplication.run(GatewayLeadsApplication.class, args); }
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayLeadsApplication.class, args);
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 
 }
