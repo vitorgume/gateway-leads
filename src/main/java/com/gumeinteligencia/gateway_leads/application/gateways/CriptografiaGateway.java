9483a81299efe28b89418891b006c11fd0980da2
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-08-12 10:47:51 -0300

    Mudando autenticação de JWT para chave de API

--- src/main/java/com/gumeinteligencia/gateway_leads/application/gateways/CriptografiaGateway.java
+++
@@ -1,6 +0,0 @@
-package com.gumeinteligencia.gateway_leads.application.gateways;
-
-public interface CriptografiaGateway {
-    String criptografar(String senha);
-    boolean validarSenha(String senha, String senhaUsuario);
-}
