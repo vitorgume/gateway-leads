53216c5aaf51e23f3394c4af7151da807d8a1a16
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-08-14 17:45:08 -0300

    Ajuste na mensagem devido a feriado

--- src/main/java/com/gumeinteligencia/gateway_leads/application/usecase/mensagem/mensagens/MensagemDirecionarPrimeiroContato.java
+++ src/main/java/com/gumeinteligencia/gateway_leads/application/usecase/mensagem/mensagens/MensagemDirecionarPrimeiroContato.java
@@ -9,7 +9,7 @@ public class MensagemDirecionarPrimeiroContato implements MensagemType{
     @Override
     public String getMensagem(String nomeVendedor, Cliente cliente) {
         return "Muito obrigado pelas informações ! Agora você será redirecionado para o(a) "
-                + nomeVendedor + ", logo entrará em contato com você ! Até...";
+                + nomeVendedor + ", logo entrará em contato com você ! Até..." + "\n Dia 15/08/2025 não estaremos atendendo devido ao feriado municipal";
     }
 
     @Override
