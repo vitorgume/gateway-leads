47e48cdf22120656e813f51e5613a697e7108222
vitorgume <vitorhvvieira@gmail.com>
2025-09-24 15:39:42 -0300

    Tentando subir API na AWS

---
+++ src/main/java/com/gumeinteligencia/gateway_leads/infrastructure/security/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.gumeinteligencia.gateway_leads.infrastructure.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final ApiKeyInterceptor apiKeyInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiKeyInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/actuator/**",
+                        "/health",
+                        "/public/**",
+                        "/webhook/**"
+                );
+    }
+}
